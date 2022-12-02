ef267f57f225dac42556883e7efe2b81381dec73
Heiko Ellerbrock <heikoellerbrock@gmail.com>
2022-12-02 16:05:45 +0100

    Header responsive

--- src/modules/navigation/components/main-menu/main-menu.component.ts
+++ src/modules/navigation/components/main-menu/main-menu.component.ts
@@ -1,22 +1,16 @@
-import {Component, Input} from '@angular/core';
-import {NavigationService} from '../../services/navigation.service';
+import { Component } from "@angular/core";
+import { NavigationService } from "../../services/navigation.service";
 
 @Component({
-  selector: 'app-main-menu',
-  templateUrl: './main-menu.component.html',
-  styleUrls: ['./main-menu.component.scss']
+  selector: "app-main-menu",
+  templateUrl: "./main-menu.component.html",
+  styleUrls: ["./main-menu.component.scss"]
 })
 export class MainMenuComponent {
-
-  /**
-   * Passed in property to change color of Button depending to transparency of complete navigation
-   */
-  @Input() isTransparent = true;
-
   constructor(private navigationService: NavigationService) {
   }
 
   get menuLinks() {
-    return this.navigationService.menuLinks
+    return this.navigationService.menuLinks;
   }
 }
--- src/modules/navigation/components/mobile-menu/mobile-menu.component.ts
+++ src/modules/navigation/components/mobile-menu/mobile-menu.component.ts
@@ -1,29 +1,19 @@
-import {Component, HostBinding, Input} from '@angular/core';
-import {NavigationService} from '../../services/navigation.service';
+import { Component, HostBinding } from "@angular/core";
+import { NavigationService } from "../../services/navigation.service";
 
 @Component({
-  selector: 'app-mobile-menu',
-  templateUrl: './mobile-menu.component.html',
-  styleUrls: ['./mobile-menu.component.scss']
+  selector: "app-mobile-menu",
+  templateUrl: "./mobile-menu.component.html",
+  styleUrls: ["./mobile-menu.component.scss"]
 })
 export class MobileMenuComponent {
-
-  @HostBinding('class.isTransparent') transparent = this.isTransparent;
-  @HostBinding('class.isOpen') open = false;
+  @HostBinding("class.isOpen") open = false;
 
   constructor(private navigationService: NavigationService) {
   }
 
   get menuLinks() {
-    return this.navigationService.menuLinks
-  }
-
-  /**
-   * Passed in property to change color of Button depending to transparency of complete navigation
-   */
-  @Input()
-  set isTransparent(val: boolean) {
-    this.transparent = val;
+    return this.navigationService.menuLinks;
   }
 
   toogleMenu() {
